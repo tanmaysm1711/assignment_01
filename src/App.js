5668b77a7565ac1d0ad3ab7fa664694918d46f3e
Tanmay Singhania <tanmaysm1711@gmail.com>
2022-11-18 14:28:57 +0530

    Initial Commit - Assignment 01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import ShoppingBasketRoundedIcon from '@mui/icons-material/ShoppingBasketRounded';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Box sx={{ 
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'space-between',
+			height: '10vh',
+			paddingInline: '4rem',
+			background: "#FFFFFF",
+			boxShadow: "0px 5px 40px rgba(0, 0, 0, 0.14)"
+		}}>
+			<h1 style={{ margin: 0 }}>The Basket</h1>
+			<ShoppingBasketRoundedIcon sx={{ width: '36px', height: '36px' }}/>
+		</Box>
+	);
 }
 
 export default App;
